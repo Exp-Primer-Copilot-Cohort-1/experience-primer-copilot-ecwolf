292a480513ffb0c6d559b2017bdf27163a82946d
Fabricio Rodríguez <frodriguez_cesen@outlook.com>
2024-01-03 16:38:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,66 +1,42 @@
-// Create a web server
-// 1. Create a new express server
-// 2. Create a few routes
-//    1. GET /comments - return all comments
-//    2. POST /comments - create a new comment
-//    3. GET /comments/:id - return a single comment
-//    4. PATCH /comments/:id - update a comment
-//    5. DELETE /comments/:id - delete a comment
-// 3. Start the server and test your work!
+// Create web server using Express
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
 
-const express = require("express");
-const app = express();
-const port = 3000;
-const comments = require("./comments");
-const { v4: uuidv4 } = require("uuid");
-
-app.use(express.json());
-
-app.get("/comments", (req, res) => {
-  res.send(comments);
+// Get comments from comments.json
+router.get('/', (req, res) => {
+  fs.readFile('./comments.json', (err, data) => {
+    if (err) {
+      console.log('Error reading comments file', err);
+      res.status(500).send('Error reading comments file');
+    } else {
+      const comments = JSON.parse(data);
+      res.send(comments);
+    }
+  });
 });
 
-app.post("/comments", (req, res) => {
-  const comment = {
-    id: uuidv4(),
-    ...req.body,
-  };
-  comments.push(comment);
-  res.send(comment);
+// Add new comment to comments.json
+router.post('/', (req, res) => {
+  console.log('req.body', req.body);
+  const newComment = req.body;
+  fs.readFile('./comments.json', (err, data) => {
+    if (err) {
+      console.log('Error reading comments file', err);
+      res.status(500).send('Error reading comments file');
+    } else {
+      const comments = JSON.parse(data);
+      comments.push(newComment);
+      fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
+        if (err) {
+          console.log('Error writing comments file', err);
+          res.status(500).send('Error writing comments file');
+        } else {
+          res.status(201).send(newComment);
+        }
+      });
+    }
+  });
 });
 
-app.get("/comments/:id", (req, res) => {
-  const comment = comments.find((comment) => comment.id === req.params.id);
-  if (comment) {
-    res.send(comment);
-  } else {
-    res.status(404).send();
-  }
-});
-
-app.patch("/comments/:id", (req, res) => {
-  const comment = comments.find((comment) => comment.id === req.params.id);
-  if (comment) {
-    comment.name = req.body.name;
-    comment.comment = req.body.comment;
-    res.send(comment);
-  } else {
-    res.status(404).send();
-  }
-});
-
-app.delete("/comments/:id", (req, res) => {
-  const commentIndex = comments.findIndex(
-    (comment) => comment.id === req.params.id
-  );
-  if (commentIndex === -1) {
-    res.status(404).send();
-  } else {
-    comments.splice(commentIndex, 1);
-    res.send(comments);
-  }
-});
-
-app.listen(port, () => {
-  console.log("Server is up on port " + port);
-});
+module.exports = router;
